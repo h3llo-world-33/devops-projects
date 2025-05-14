45be28e6635e586b938ee9ffe442d5fa3bfcb1d4
h3llo-world-33 <ibk.teksphere@gmail.com>
2025-05-14 12:31:42 +0100

    packer built jenkins controller ami id

--- 01-jenkins-setup/terraform/efs/main.tf
+++ 01-jenkins-setup/terraform/efs/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 module "efs_module" {
   source = "../modules/efs"
-  vpc_id     = "vpc-0dd5030c7319f3b8c"
-  subnet_ids = ["subnet-0e21b32261276eb5c", "subnet-0f745b45356f7f113", "subnet-0deed58b2f81bebae"]
+  vpc_id     = "vpc-0493a3637ed6bb75d"
+  subnet_ids = ["subnet-06163c3a7580b52a7", "subnet-0edab1f6a39382024", "subnet-012dc7bc82c645058"]
 }
